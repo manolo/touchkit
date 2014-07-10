20f55c47e84d62d454ec34f5b30ecde9408a10d0
Manolo Carrasco <manolo@vaadin.com>
2014-07-10 09:13:44 +0200

    publishing snapshot jars
    Change-Id: If6587289e94852e60b132a4c92ccc7794f9a25d7

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/vcom/navigation/NavigationViewState.java
+++
@@ -1,7 +0,0 @@
-package com.vaadin.addon.touchkit.gwt.client.vcom.navigation;
-
-import com.vaadin.shared.AbstractComponentState;
-
-public class NavigationViewState extends AbstractComponentState {
-    public int scrollPosition;
-}
